77d1a6365516ea52d4a62de46632d0201593027e
toby676 <tjcmui@gmail.com>
2016-06-29 12:51:14 +0100

    refactored user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,7 @@ class User
 
   def self.authenticate(email, password)
     user = first(email: email)
-    if user && BCrypt::Password.new(user.password_digest) == password
-      user
-    else
-      nil
-    end
+    user if user && BCrypt::Password.new(user.password_digest) == password
   end
 
   private
