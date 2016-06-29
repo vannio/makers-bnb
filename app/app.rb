b4eb0b9e3665fcf1bdd384eb228e4ec9f1db253b
Van Le <singularity@vannio.co.uk>
2016-06-29 11:00:47 +0100

    Removed comments

--- app/app.rb
+++ app/app.rb
@@ -10,6 +10,5 @@ require_relative "server"
 require_relative "controllers/index"
 
 class Makersbnb < Sinatra::Base
-  # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,6 @@ require "data_mapper"
 require "dm-validations"
 require "dm-postgres-adapter"
 
-local_postgres = "postgres://localhost/makersbnb_#{ ENV["RACK_ENV"] }"
-DataMapper.setup(:default, ENV["DATABASE_URL"] || local_postgres)
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/makersbnb_#{ ENV["RACK_ENV"] }")
 DataMapper.finalize
 DataMapper.auto_upgrade!
