915e1e2b58dc68fee5e719188fbb07b2f1b23699
toby676 <tjcmui@gmail.com>
2016-06-30 17:59:17 +0100

    bookingchecker refactored

--- app/models/booking_checker.rb
+++ app/models/booking_checker.rb
@@ -1,5 +1,12 @@
 class BookingChecker
 
+  def valid?(start_date, end_date, existing_booking_dates, booking_date)
+    is_in_range?(start_date, end_date, booking_date) &&
+    is_not_already_booked?(existing_booking_dates, booking_date)
+  end
+
+  private
+
   def is_in_range?(start_date, end_date, booking_date)
     (start_date < booking_date && booking_date < end_date)
   end
--- spec/booking_checker_spec.rb
+++ spec/booking_checker_spec.rb
@@ -6,24 +6,17 @@ describe BookingChecker do
   let(:booking_date) { "2016-01-02" }
   let(:existing_booking_dates) { ["2016-04-17", "2016-04-18", "2016-04-20"] }
 
-  describe "#is_in_range?" do
-    it "validates if the date is within the owner's specified range" do
-      expect(booking_checker.is_in_range?(start_date, end_date, booking_date)).to be true
+  describe "#valid?" do
+    it "returns true with a valid booking date" do
+      expect(booking_checker.valid?(start_date, end_date, existing_booking_dates, booking_date)).to be true
     end
 
-    it "invalidates if the date is not within the owner's specified range" do
-      expect(booking_checker.is_in_range?(start_date, end_date, "2019-01-01")).to be false
+    it "returns false with an invalid booking date" do
+      expect(booking_checker.valid?(start_date, end_date, existing_booking_dates, "2019-01-01")).to be false
     end
 
-  end
-
-  describe "#is_not_already_booked?" do
-    it "checks that the date has not already been booked by another user" do
-      expect(booking_checker.is_not_already_booked?(existing_booking_dates, booking_date)).to be true
-    end
-
-    it "checks that the date has already been booked by another user" do
-      expect(booking_checker.is_not_already_booked?(existing_booking_dates, "2016-04-17")).to be false
+    it "returns false with an invalid booking date" do
+      expect(booking_checker.valid?(start_date, end_date, existing_booking_dates, "2016-04-17")).to be false
     end
 
   end
