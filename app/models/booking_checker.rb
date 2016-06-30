1a7e65bac390f4804d80f3a931ebae37c335a899
toby676 <tjcmui@gmail.com>
2016-06-30 17:48:47 +0100

    added anti-double-booking-method

--- app/models/booking_checker.rb
+++ app/models/booking_checker.rb
@@ -1,7 +1,11 @@
 class BookingChecker
 
-  def validate_range(start_date, end_date, booking_date)
+  def is_in_range?(start_date, end_date, booking_date)
     (start_date < booking_date && booking_date < end_date)
+  end
+
+  def is_not_already_booked?(existing_booking_dates, booking_date)
+    !existing_booking_dates.include?(booking_date)
   end
 
 end
--- spec/booking_checker_spec.rb
+++ spec/booking_checker_spec.rb
@@ -4,14 +4,26 @@ describe BookingChecker do
   let(:start_date) { "2016-01-01" }
   let(:end_date) { "2017-01-01" }
   let(:booking_date) { "2016-01-02" }
+  let(:existing_booking_dates) { ["2016-04-17", "2016-04-18", "2016-04-20"] }
 
-  describe "#validate_range" do
+  describe "#is_in_range?" do
     it "validates if the date is within the owner's specified range" do
-      expect(booking_checker.validate_range(start_date, end_date, booking_date)).to be true
+      expect(booking_checker.is_in_range?(start_date, end_date, booking_date)).to be true
     end
 
     it "invalidates if the date is not within the owner's specified range" do
-      expect(booking_checker.validate_range(start_date, end_date, "2019-01-01")).to be false
+      expect(booking_checker.is_in_range?(start_date, end_date, "2019-01-01")).to be false
+    end
+
+  end
+
+  describe "#is_not_already_booked?" do
+    it "checks that the date has not already been booked by another user" do
+      expect(booking_checker.is_not_already_booked?(existing_booking_dates, booking_date)).to be true
+    end
+
+    it "checks that the date has already been booked by another user" do
+      expect(booking_checker.is_not_already_booked?(existing_booking_dates, "2016-04-17")).to be false
     end
 
   end
